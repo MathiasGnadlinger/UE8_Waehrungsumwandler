947bbd6cd4a33ce47aac6e7c9b7541206b8f6dc4
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-11-16 23:58:25 +0100

    Fertigstellung

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
-public class Controller {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+/**
+ * @author Mathias Gnadlinger
+ * @version 8, 16.11.2020
+ */
+public class Controller extends CurrencyConverter
+{
+    @FXML
+    TextField tf1 = new TextField();
+    @FXML
+    TextField tf2 = new TextField();
+    @FXML
+    Button b1 = new Button();
+
+    double euro;
+
+    public void doConvertion()
+    {
+
+        EventHandler<MouseEvent> Convert = new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent e)
+            {
+                try
+                {
+                    euro = Double.parseDouble(tf1.getText());           //Wert wird herausgeschrieben und zum Rechnen verwendet
+                } catch (Exception ex) {
+                    System.out.printf("Falsche Eingabe%n");
+                }
+                tf2.setText(String.valueOf(euroToYen(euro)));           //Ausgabe in dem Textfeld
+            }
+        };
+        b1.addEventHandler(MouseEvent.MOUSE_CLICKED,Convert);
+    }
 }
---
+++ src/sample/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class CurrencyConverter
+{
+    public double euroToYen(double euro)
+    {
+        double Yen = euro * 124;
+
+        return Yen;
+    }
+}
