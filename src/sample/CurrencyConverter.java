1eb7fc44c88bb935b5b4add2d3a23c8cf80a0d24
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-11-17 10:52:26 +0100

    Finalisierung

--- src/sample/CurrencyConverter.java
+++ src/sample/CurrencyConverter.java
@@ -1,5 +1,8 @@
 package sample;
-
+/**
+ * @author Mathias Gnadlinger
+ * @version 8, 16.11.2020
+ */
 public class CurrencyConverter
 {
     public double euroToYen(double euro)
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+/**
+ * @author Mathias Gnadlinger
+ * @version 8, 16.11.2020
+ */
 public class Main extends Application
 {
 
